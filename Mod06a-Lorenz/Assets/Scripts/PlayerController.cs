47dccea63142b4644088f7bddec53fb4cf1cb14b
juliaclorenz <julialorenz17@icloud.com>
2023-03-07 13:46:25 -0600

    moving car
    :D

---
+++ Mod06a-Lorenz/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Veritcal");
+
+        //Move Vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+
+
+    }
+}
